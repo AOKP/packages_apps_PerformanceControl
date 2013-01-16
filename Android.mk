9ec910b20f7f1d64495b07cf25f319b89b35d60e
xoomdev <xoomdevnet@gmail.com>
2013-01-15 19:14:22 -0600

    Add Android.mk to enable building in the Android source tree.

---
+++ Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := \
+        $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := PerformanceControl
+
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
+
+include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
